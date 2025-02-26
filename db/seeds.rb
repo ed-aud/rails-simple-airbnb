c91fa4de07a1ff6ffa7751ebb9bc79052a56a966
Ed Audland <edaudland@gmail.com>
2025-02-26 16:29:00 +0000

    Rails Simple Airbnb Challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,40 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# 1. Clean the database
+puts "Cleaning database..."
+Flat.destroy_all
+
+# 2. Create the instances
+puts "Creating restaurants..."
+Flat.create!(
+  name: 'Light & Spacious Garden Flat in London',
+  address: 'London, UK',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Small & Dingy Basement Flat in London',
+  address: 'Bath, UK',
+  description: 'This is a hovel. Do not stay here',
+  price_per_night: 15,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Cute Family Townhouse with Roof Terrace',
+  address: 'Manchester, UK',
+  description: 'A great spot for a family in the heart of Manchester',
+  price_per_night: 125,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Country Mansion with Splendid Gardens',
+  address: 'Yorkshire, UK',
+  description: 'An old English manor estate. No stag or hen dos allowed!',
+  price_per_night: 350,
+  number_of_guests: 8
+)
+
+# 3. Display a message
+puts "Finished! Created #{Flat.count} flats."
